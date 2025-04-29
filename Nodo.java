d91657945146cbb7cc5ee2d6fdd3adcb174471ad
isabellacasta198 <isabella.castano03@usc.edu.co>
2025-04-29 15:51:04 -0500

    Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,12 @@
+public class Nodo {
+    Palabra dato;
+    Nodo izquierdo;
+    Nodo derecho;
+
+    public Nodo(Palabra dato){
+        this.dato= dato;
+        this.izquierdo= null;
+        this.derecho=null;
+
+    }
+}
